294b73c7a93aa41cc725d79766de5f3b7d761811
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2020-12-29 13:02:44 -0500

    Added Enigma java submodule.

---
+++ EnigmaMachine/src/main/java/com/mindtedtech/enigmamachine/MyClass.java
@@ -0,0 +1,5 @@
+package com.mindtedtech.enigmamachine;
+
+public class MyClass
+{
+}
---
+++ EnigmaMachine/src/main/java/com/mindtedtech/enigmamachine/interfaces/Wiring.java
@@ -0,0 +1,12 @@
+package com.mindtedtech.enigmamachine.interfaces;
+
+/**
+ * @author Eliezer Meth
+ * Start Date: 2020.09.13
+ */
+
+public interface Wiring
+{
+    int get(int letter);
+    int indexOf(int letter);
+}
