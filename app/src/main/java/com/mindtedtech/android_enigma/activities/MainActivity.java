bdc58d214af56466d9701b3965822bea4c3c70df
Dov <dovpechman@gmail.com>
2020-12-31 21:37:40 -0500

    messages should know be destroyed only when the app is completely closed

--- app/src/main/java/com/mindtedtech/android_enigma/activities/MainActivity.java
+++ app/src/main/java/com/mindtedtech/android_enigma/activities/MainActivity.java
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity
     private String saveMessage;
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
 
         keepDataIfSavedMessagesWasOn();
     }
